9470a29b7787914708a999732399a62aa1831512
IsolatedOni <159647406+IsolatedOni@users.noreply.github.com>
2026-03-29 16:41:21 +0000

    Edited Code

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,13 +1,16 @@
-package cardDealer;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 public class DeckOfCards{
-  // private String[] descriptionSuit = {"Clubs","Hearts","Spades","Diamonds"};
-	private String[] descriptionSuit = {"1","2","3","4"};
-  // private String[] descriptionRank = {"Ace","King","Queen","Jack","Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two"};
+  private String[] descriptionSuit2 = {"Clubs","Hearts","Spades","Diamonds"};
+	
+  private String[] descriptionSuit = {"1","2","3","4"};
+  
+  private String[] descriptionRank2 = {"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
+
   private String[] descriptionRank = {"1","2","3","4","5","6","7","8","9","10","11","12","13"};
-	private String[] hand = new String[5];
+	private static String[] hand = new String[5];
 	public ArrayList<String> cards = new ArrayList<String>();
   
   public DeckOfCards(){
@@ -25,7 +28,7 @@ public class DeckOfCards{
   public void shuffle() {
 		int random = 0;
 		int exe = 0;
-		int size = cards.length();
+		int size = cards.size();
 		
 	  for(int i = 0; i < 52; i++) {
 			exe = (int) (Math.random() * size);
@@ -41,18 +44,24 @@ public class DeckOfCards{
       cards.remove(i);    
     } 
 		
+    for(int i = 0; i < 5; i++){
+      // System.out.println(hand[i]);
+      System.out.println(descriptionRank2[Integer.parseInt(getRank(i)) - 1] + " of " + descriptionSuit2[Integer.parseInt(hand[i].substring(hand[i].length() - 1, hand[i].length())) - 1]);
+    }
    String user = "";
 
    System.out.println("If you are satsified with your hand type exit else type remove.");
-  user = scan.nextLine();
+    user = scan.nextLine();
     if(user.equals("remove")){
       System.out.println("Type out the numbers in this format \" 1 2 3 4 5 \" Do not type cards you dont want to remove.");
       int x = 0;
       int y = 1;
       int counter = 0;
-      for(int l = 0; l < user.length(); i++){
-        String z = user.substring(x,y)
-        if(z.equals(" "){
+      user = scan.nextLine();
+
+      for(int l = 0; l < user.length(); l++){
+        String z = user.substring(x,y);
+        if(z.equals(" ")){
           counter++;
         }
         x++;
@@ -62,11 +71,16 @@ public class DeckOfCards{
       y = 1;
       
       for(int k = 0; k < user.length() - counter; k++){
-        hand[Integer.parseInt(user.substring(x,y)] = cards.get(0));
+        hand[Integer.parseInt(user.substring(x,y)) - 1] = cards.get(0);
         cards.remove(0);
         x += 2;
         y += 2;
       }
+
+    for(int i = 0; i < 5; i++){
+      // System.out.println(hand[i]);
+      System.out.println(descriptionRank2[Integer.parseInt(getRank(i)) - 1] + " of " + descriptionSuit2[Integer.parseInt(hand[i].substring(hand[i].length() - 1, hand[i].length())) - 1]);
+    }
     
 	}
   }
@@ -84,13 +98,13 @@ public class DeckOfCards{
     
   }
 
-  public String getRank(int num){
+  public static String getRank(int num){
     String cardRank = hand[num];
     String cR = "";
-    if(cardRank.substring(1,2) != " "){
-      cR = cardRank.substring(0,2);
-    } else {
+    if(cardRank.substring(1,2).equals((" "))){
       cR = cardRank.substring(0,1);
+    } else {
+      cR = cardRank.substring(0,2);
     }
 
     return cR;
@@ -98,8 +112,8 @@ public class DeckOfCards{
 
   public static boolean isFlush(){
     String suit = hand[0].substring(hand[0].length() - 2, hand[0].length() - 1);
-    for(int i = 1; i < hand.lenght(); i++){
-      if(hand[i].substring(hand.length() - 2, hand.length() - 1) != suit){
+    for(int i = 1; i < 5; i++){
+      if(hand[i].substring(hand[i].length() - 2, hand[i].length() - 1) != suit){
         return false;   
       }
     }
@@ -108,7 +122,7 @@ public class DeckOfCards{
 
   // Implements the method as if hand is an array not an ArrayList
   public static boolean isStraight(){
-    for(int i = 1;  i < hand.length(); i++){
+    for(int i = 1;  i < hand.length; i++){
        if(Integer.parseInt(hand[i].getRank()) != Integer.parseInt(hand[i - 1].getRank()) + 1){
          return false;
         }
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main{
   public static void main(String[] args){
-  
+    DeckOfCards alpha = new DeckOfCards();
+     alpha.createDeck();
+     alpha.shuffle();
+     alpha.drawHand();
+     alpha.checkHand();
   }
 }
