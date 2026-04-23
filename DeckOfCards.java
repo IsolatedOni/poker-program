48cc7cf69efd2a6d1234cc07c225bfefb82381a7
Eric-Samuel Takouda <159647406+IsolatedOni@users.noreply.github.com>
2026-04-23 15:03:41 +0000

    Updated pairorkind class to check for other formats of winning

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -49,11 +49,12 @@ public class DeckOfCards{
       System.out.println(descriptionRank2[Integer.parseInt(getRank(i)) - 1] + " of " + descriptionSuit2[Integer.parseInt(hand[i].substring(hand[i].length() - 1, hand[i].length())) - 1]);
     }
    String user = "";
-
-   System.out.println("If you are satsified with your hand type exit else type remove.");
+  
+   System.out.println();
+   System.out.println("If you are satsified with your hand type exit else type remove. \n");
     user = scan.nextLine();
     if(user.equals("remove")){
-      System.out.println("Type out the numbers in this format \" 1 2 3 4 5 \" Do not type cards you dont want to remove.");
+      System.out.println("Type out the numbers in this format \" 1 2 3 4 5 \" Do not type cards you dont want to remove.\n");
       int x = 0;
       int y = 1;
       int counter = 0;
@@ -96,6 +97,7 @@ public class DeckOfCards{
           // System.out.println(hand[i]);
           System.out.println(descriptionRank2[Integer.parseInt(getRank(i)) - 1] + " of " + descriptionSuit2[Integer.parseInt(hand[i].substring(hand[i].length() - 1, hand[i].length())) - 1]);
         }
+
     }
 
   public void checkHand(){
@@ -103,11 +105,21 @@ public class DeckOfCards{
     boolean op = false;
     boolean tp = false;
     boolean tk = false;
-    boolean s = false;
+    boolean s = isStraight();
     boolean f = isFlush();
     boolean fh = false;
     boolean fk = false;
     boolean rf = false;
+
+    if(s == true && f == true){
+      System.out.println("You drew a straight flush");
+    } else if(s == true){
+      System.out.println("You drew a straight");
+    } else if(f == true){
+      System.out.println("You drew a flush");
+    }
+    pairOrKind();
+
     
   }
 
@@ -126,11 +138,11 @@ public class DeckOfCards{
   public static boolean isFlush(){
     String suit = hand[0].substring(hand[0].length() - 2, hand[0].length() - 1);
     for(int i = 1; i < 5; i++){
-      if(hand[i].substring(hand[i].length() - 2, hand[i].length() - 1) != suit){
-        return false;   
+      if(hand[i].substring(hand[i].length() - 2, hand[i].length() - 1) == suit){
+        return true;   
       }
     }
-    return true;
+    return false;
   }
 
   // Implements the method as if hand is an array not an ArrayList
@@ -143,13 +155,13 @@ public class DeckOfCards{
     return true;
   }
 
-	public static String pairOrKind(){
+	public static void pairOrKind(){
 		int counter = 0;
 		int counter2 = 0;
 		int index1 = 0;
 		int index2 = 1;
-		int x = 1
-		for(int k = 0; k < 4; k++;){
+		int x = 1;
+		for(int k = 0; k < 4; k++){
 			if(Integer.parseInt(getRank(index1)) == Integer.parseInt(getRank(index2))){
 				switch(x){
 					case 1:
@@ -163,22 +175,25 @@ public class DeckOfCards{
 				}		
 			
 			} else {
-				index1 == index 2;
-				index2 == index2 + 1;
+				index1 = index2;
+				index2 = index2 + 1;
 				x = 2;
 			}
 		}
 		if((counter == 1 && counter2 == 0) || (counter == 0 && counter2 == 1)){
-			return "Pair";
+			System.out.println("\nPair");
 		}
 		if(counter == 1 && counter2 == 1){
-			return "Two Pair";
+			System.out.println("\nTwo Pair");
 		}
 		if((counter == 2 && counter2 == 0) || (counter == 0 && counter2 == 2)){
-			return "Three of a Kind";
+			System.out.println("\nThree of a Kind");
 		}
 		if((counter == 3 && counter2 == 0) || (counter == 0 && counter2 == 3)){
-			return "Four of a kind";
+			System.out.println("\nFour of a Kind");
+		}
+    if((counter == 1 && counter2 == 2) || (counter == 2 && counter2 == 1)){
+			System.out.println("\nFull House");
 		}
 	}
 }
